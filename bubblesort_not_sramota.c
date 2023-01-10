f41e4741ad8589c46cba6aec0f415a67e10c4f17
Viktor-lashin <72409725+Viktor-lashin@users.noreply.github.com>
2023-01-10 20:20:35 +0300

    Create bubblesort_not_sramota.c

---
+++ bubblesort_not_sramota.c
@@ -0,0 +1,14 @@
+void bubblesort(unsigned long nel,int (*compare)(unsigned long i, unsigned long j),void (*swap)(unsigned long i, unsigned long j))
+{
+    int found=0;
+    for(int i=1;i<nel;i++)
+        if(compare(i-1,i)==1)
+            swap(i-1,i);
+    nel--;
+    for(int i=nel-1;i>=1;i--)
+        if(compare(i-1,i)==1){
+            swap(i-1,i);
+            found=1;
+        }
+    if(found) bubblesort(nel,compare,swap);
+}
